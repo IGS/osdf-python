e80aa04870f5c3777a6e7f150c708695921ec691
victor73 <victor73>
2015-05-08 04:07:05 -0400

    Added python code.

---
+++ osdf.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+
+import base64
+import httplib
+import json
+import os
+import sys
+from request import HttpRequest
+
+class OSDF:
+    """
+    Communicates with an OSDF server's REST interface to facilitate several
+    operations (node creation, deletion, queries, etc.)
+    """
+
+    def __init__(self, server, username, password, port=8123):
+        self.server = server
+        self.port = port
+        self.username = username
+        self.password = password
+        self._request = HttpRequest(server, username, password, port=port)
+
+    def edit_node(self, json_data):
+        """
+        Updates a node with the provided data
+        """
+        # Get the node id from json_data
+        if 'id' not in json_data:
+            raise Exception("No node id in the provided JSON.")
+
+        node_id = json_data['id']
+
+        json_str = json.dumps(json_data);
+
+        osdf_response = self._request.put("/nodes/" + node_id, json_str)
+
+        if osdf_response["code"] != 200:
+            headers = osdf_response['headers']
+
+            if 'x-osdf-error' in headers:
+                msg = "Unable to edit node document. Reason: " \
+                      + headers['x-osdf-error']
+                raise Exception(msg)
+            else:
+                raise Exception("Unable to edit node document.")
+
+    def get_info(self):
+        """
+        Retrieve's the OSDF server's information/contact document
+        """
+        osdf_response = self._request.get("/info")
+
+        info = json.loads( osdf_response['content'] )
+
+        return info
+
+    def get_node(self, node_id):
+        """
+        Retrieves an OSDF node given the node's ID
+
+        Returns the parsed form of the JSON document for the node
+        """
+        osdf_response = self._request.get("/nodes/" + node_id)
+
+        if osdf_response["code"] != 200:
+            headers = osdf_response['headers']
+
+            if 'x-osdf-error' in headers:
+                msg = "Unable to retrieve node document. Reason: " \
+                      + headers['x-osdf-error']
+            else:
+                msg = "Unable to retrieve node document."
+
+            raise Exception(msg)
+
+        data = json.loads( osdf_response['content'] )
+
+        return data
+
+    def get_schema(self, namespace, schema_name):
+        """
+        Retrieves a namespace's document schema
+
+        Returns the parsed form of the JSON-Schema document
+        """
+        url = '/namespaces/%s/schemas/%s' % (namespace, schema_name)
+
+        osdf_response = self._request.get(url)
+
+        if osdf_response["code"] != 200:
+            headers = osdf_response['headers']
+
+            if 'x-osdf-error' in headers:
+                msg = "Unable to retrieve schema document. Reason: " \
+                      + headers['x-osdf-error']
+            else:
+                msg = "Unable to retrieve schema document."
+
+            raise Exception(msg)
+
+        schema_data = json.loads( osdf_response['content'] )
+
+        return schema_data
+
+    def get_aux_schema(self, namespace, aux_schema_name):
+        """
+        Retrieves an auxiliary schema
+
+        Returns the parsed form of the auxiliary schema JSON
+        """
+        url = '/namespaces/%s/schemas/aux/%s' % (namespace, aux_schema_name)
+
+        osdf_response = self._request.get(url)
+
+        if osdf_response["code"] != 200:
+            headers = osdf_response['headers']
+
+            if 'x-osdf-error' in headers:
+                msg = "Unable to retrieve schema document. Reason: " \
+                      + headers['x-osdf-error']
+            else:
+                msg = "Unable to retrieve schema document."
+
+            raise Exception(msg)
+
+        aux_schema_data = json.loads( osdf_response['content'] )
+
+        return aux_schema_data
+
+    def insert_node(self, json_data):
+        """
+        Inserts a node with the provided data into OSDF
+
+        Returns the node ID upon successful insertion.
+        """
+        json_str = json.dumps(json_data);
+
+        osdf_response = self._request.post("/nodes", json_str)
+        node_id = None
+
+        headers = osdf_response["headers"]
+
+        if osdf_response["code"] == 201:
+            if 'location' in headers:
+                node_id = headers['location'].split('/')[-1]
+            else:
+                raise Exception("No location header for the newly inserted node.")
+        else:
+            if 'x-osdf-error' in headers:
+                msg = "Unable to insert node document. Reason: " + headers['x-osdf-error']
+            else:
+                msg = "Unable to insert node document."
+
+            raise Exception(msg)
+
+        return node_id
+
+    def delete_node(self, node_id):
+        """
+        Deletes the specified node from OSDF.
+        """
+        osdf_response = self._request.delete("/nodes/" + node_id)
+
+        if osdf_response['code'] != 200:
+            if 'x-osdf-error' in headers:
+                msg = "Unable to delete node document. Reason: " + headers['x-osdf-error']
+            else:
+                msg = "Unable to delete node document."
+
+            raise Exception(msg)
+
+    def validate_node(self, json_data):
+        """
+        Report whether a node document validates against OSDF and it's notion
+        of what that node should look like according to any registered schemas.
+
+        Returns a tuple with the first value holding a boolean of whether the
+        document validated or not. The second value contains the error message
+        if the document did not validate.
+        """
+        json_str = json.dumps(json_data);
+        url = "/nodes/validate"
+
+        osdf_response = self._request.post(url, json_str)
+        headers = osdf_response["headers"]
+        valid = False
+
+        error_msg = None
+
+        if osdf_response["code"] != 200:
+            if 'x-osdf-error' in headers:
+                error_msg = headers['x-osdf-error']
+            else:
+                error_msg = "Unknown"
+        else:
+            valid = True
+
+        return (valid, error_msg)
+
+
+    def query(self, namespace, query, page=1):
+        """
+        Issue a query against OSDF. Queries are expressed in JSON form using
+        the ElasticSearch Query DSL.
+
+        Returns the specified page of results.
+        """
+        url = "/nodes/query/%s/page/%s" % (namespace, str(page))
+
+        osdf_response = self._request.post(url, query)
+
+        if osdf_response["code"] != 200 and osdf_response["code"] != 206:
+            headers = osdf_response["headers"]
+
+            if 'x-osdf-error' in headers:
+                msg = "Unable to query namespace %s. Reason: %s" % (namespace, headers['x-osdf-error'])
+            else:
+                msg = "Unable to query namespace."
+
+            raise Exception(msg)
+
+        data = json.loads( osdf_response['content'] )
+
+        return data
+
+    def query_all_pages(self, namespace, query):
+        """
+        Issue a query against OSDF, as in the query() method, but retrieves
+        ALL results by aggregating all the available pages of results. Use with
+        caution, as this may consume a lot of memory with large result sets.
+        """
+        more_results = True
+        page = 1
+        cumulative_results = []
+
+        while more_results:
+           results = self.query(namespace, query, page)
+
+           cumulative_results.extend(results['results'])
+
+           if results['result_count'] > 0:
+               page += 1
+           else:
+               more_results = False
+
+        results['results'] = cumulative_results
+        results['result_count'] = len(results['results'])
+        del results['page']
+
+        return results;
+
+    def create_osdf_node(self, namespace, node_type, domain_json, linkage={}, read="all", write="all"):
+        node_json = { 'ns': namespace,
+                      'acl': { 'read': [ read ], 'write': [ write ] },
+                      'linkage': linkage,
+                      'meta': domain_json,
+                      'node_type': node_type }
+
+        return node_json
---
+++ request.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+import httplib
+import base64
+
+class HTTPStatusException(Exception):
+
+    def __init__(self, status, message):
+        self.status = status
+        self.message = message
+
+    def __str__(self):
+        return "Error [%s]: %s" % (self.status, self.message)
+
+class HttpRequest:
+
+    def __init__(self, server, username, password, port=8123):
+        self.server = server
+        self.port = port
+        self.username = username
+        self.password = password
+
+    def delete(self, resource):
+        conn = httplib.HTTPConnection(self.server, self.port)
+
+        conn.putrequest("DELETE", resource)
+        self._set_auth_header(conn)
+        conn.endheaders()
+        resp = conn.getresponse()
+        content = resp.read()
+
+        raw_headers = resp.getheaders()
+
+        for header in raw_headers:
+            header_name = header[0]
+            header_value = header[1]
+            headers[header_name] = header_value
+
+        results = { "headers": headers,
+                    "content": content,
+                    "code": resp.status
+                  }
+
+        return results
+
+    def get(self, resource):
+        conn = httplib.HTTPConnection(self.server, self.port)
+        conn.putrequest("GET", resource)
+        self._set_auth_header(conn)
+        conn.endheaders()
+        resp = conn.getresponse()
+        content = resp.read()
+
+        raw_headers = resp.getheaders()
+
+        for header in raw_headers:
+            header_name = header[0]
+            header_value = header[1]
+            headers = {}
+            headers[header_name] = header_value
+
+        results = { "headers": headers,
+                    "content": content,
+                    "code": resp.status
+                  }
+
+        return results
+
+    def put(self, resource, data):
+        conn = httplib.HTTPConnection(self.server, self.port, True)
+        conn.putrequest("PUT", resource)
+        self._set_auth_header(conn)
+        conn.putheader("Content-Length", "%d" % len(data))
+        conn.endheaders()
+
+        conn.send(data)
+
+        resp = conn.getresponse()
+        raw_headers = resp.getheaders()
+        content = resp.read()
+
+        resp_headers = {}
+
+        for header in raw_headers:
+            header_name = header[0]
+            header_value = header[1]
+            resp_headers[header_name] = header_value
+
+        results = { "headers": resp_headers,
+                    "content": content,
+                    "code": resp.status
+                  }
+
+        return results
+
+    def post(self, resource, data):
+        conn = httplib.HTTPConnection(self.server, self.port, True)
+
+        conn.putrequest("POST", resource)
+        self._set_auth_header(conn)
+        conn.putheader("Content-Length", "%d" % len(data))
+        conn.endheaders()
+
+        conn.send(data)
+
+        resp = conn.getresponse()
+        raw_headers = resp.getheaders()
+        content = resp.read()
+
+        resp_headers = {}
+
+        for header in raw_headers:
+            header_name = header[0]
+            header_value = header[1]
+            resp_headers[header_name] = header_value
+
+        results = { "headers": resp_headers,
+                    "content": content,
+                    "code": resp.status
+                  }
+
+        return results
+
+    def _set_auth_header(self, connection):
+        # We don't use the base64.encodestring() method here becuase it automatically adds
+        # newlines
+        base64string = "Basic " + base64.b64encode('%s:%s' % (self.username, self.password))
+        connection.putheader("Authorization", base64string)
