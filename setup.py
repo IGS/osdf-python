469401ea686841a83994ab3c77089e10e7c5ea6e
Victor <victor73@gmail.com>
2015-06-16 17:43:26 -0400

    New packaging related files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from distutils.core import setup
+
+# Utility function to read files. Used for the long_description.
+def read(fname):
+      return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='osdf-python',
+      description='Python client to Open Science Data Framework (OSDF) REST servers.',
+      long_description=read('README.md'),
+      version='0.1',
+      py_modules=['osdf', 'request'],
+      author='Victor Felix',
+      author_email='victor73@gmail.com',
+      url='http://osdf.igs.umaryland.edu',
+      license='MIT',
+      requires=['httplib'],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: UNIX",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+      ]
+     )
